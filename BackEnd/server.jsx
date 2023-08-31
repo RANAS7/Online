003f750e457e203fc10f00c0063a7bc49a0f4c99
Deepak Rana <130432877+RANAS7@users.noreply.github.com>
2023-08-31 19:12:13 +0545

    code upload

---
+++ BackEnd/server.jsx
@@ -0,0 +1,47 @@
+const express = require("express");
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "signup"
+})
+
+app.post('/signUp', (req, res) => {
+    const sql = "   INSERT INTO login (`username`,`email`,`password`) VALUES(?)";
+    const values = [
+        req.body.username,
+        req.body.email,
+        req.body.password
+    ]
+    db.query(sql, [values], (err, data) => {
+        if(err) {
+            return res.json("Error");
+        }
+        return res.json(data);
+    })
+})
+
+app.post('/login', (req, res) => {
+    const sql = "SELECT * FROM login WHERE `email` = ? AND  `password` = ?";
+        db.query(sql, [req.body.username, req.body.password], (err, data) => {
+        if(err) {
+            return res.json("Error");
+        }
+        if(data.length > 0) {
+            return res.json("Success");
+        } else{
+            return res.json("faile");
+        }
+    })
+})
+
+app.listen(80, ()=> {
+    console.log("listening");
+})
